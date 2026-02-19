f041a8daa0003a0187840f30b6693f8ae8be82d2
NedaTawakoli <tawakolineda@gmail.com>
2026-02-19 23:06:56 +0430

    imgSlider

--- home.js
+++ home.js
@@ -2,8 +2,12 @@ const btn = document.querySelector('button');
 const img = document.querySelector('img');
 btn.addEventListener("click",changeImage);
 let list = [
-    "C:\Users\Nafisa Tawakoli\Pictures\girals/",
-    ""
+    "FB_IMG_1701430178341.jpg",
+    "FB_IMG_1701430176337.jpg",
+    "FB_IMG_1700724137222.jpg",
+    "FB_IMG_1700724147699.jpg",
+    "FB_IMG_1703871468060.jpg",
+    "FB_IMG_1701425533671 (2).jpg"
 ]
 img.setAttribute('src',list[0]);
 let counter =0;
