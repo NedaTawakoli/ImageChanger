0e15e1d70b17d96e0aa476ddaf2deb8e809497e3
NedaTawakoli <tawakolineda@gmail.com>
2026-03-09 23:52:14 +0430

    export znd import added

---
+++ Import.js
@@ -0,0 +1,12 @@
+import { list } from "./export"
+import { Greething } from "./export";
+listOfStudent.map(items=>{
+    console.log(listOfStudent.name);
+});
+Greething();
+const person={
+    name:"liala",
+    lastName:"naqi",
+    age:23
+}
+const {name,lastName,age} = person;
---
+++ export.js
@@ -0,0 +1,47 @@
+export let list =[
+    {
+        name:"Ali",
+        lastName:"Ahmadi",
+        Age:18,
+        gender:"male"
+    },
+    {
+        name:"Sadaf",
+        lastName:"Qurbani",
+        Age:16,
+        gender:"Female"
+    },
+    {
+        name:"Alina",
+        lastName:"Ahmad khan",
+        Age:18,
+        gender:"Female"
+    },
+    {
+        name:"Alia",
+        lastName:"Bahrami",
+        Age:20,
+        gender:"Female"
+    },
+    {
+        name:"Arman",
+        lastName:"Mohhamdi",
+        Age:19,
+        gender:"male"
+    },
+    {
+        name:"Setara",
+        lastName:"Mahomdi",
+        Age:17,
+        gender:"Female"
+    },
+    {
+        name:"Nargis",
+        lastName:"Taheri",
+        Age:18,
+        gender:"Female"
+    },
+];
+export default function Greething(){
+    console.log("Hello everyone i hope your good");
+}
